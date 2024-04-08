f2b2516d05983c90f7a66c5246cfe4889b2f4c9c
itzdoxa <153353785+itzdoxa@users.noreply.github.com>
2024-04-08 19:18:06 +0530

    used for loop

--- script.js
+++ script.js
@@ -14,78 +14,77 @@ function getComputerChoice () {
        x  = "scissors";
     break;
      }
- console.log("computer chose " + x);
+ 
  return x;
 }
 
 let computerScore = 0;
 let playerScore = 0;
-let round = 1;
 
 function playRound (playerSelection, computerSelection) {
             if (playerSelection === computerSelection) {
-               round += 1;
+               
                return "it's a tie";
             }
             else if (playerSelection == "rock" && computerSelection == "paper") {
               computerScore += 1;
-              round += 1;
+              
               return "you lose, paper defeats rock";
             }
             else if (playerSelection == "rock" && computerSelection == "scissors") {
                playerScore += 1;
-               round += 1;
+               
                return "you win, rock defeats scissors";
             }
             else if (playerSelection == "paper" && computerSelection == "rock") {
                playerScore += 1;
-               round += 1;
+               
                return "you win, paper defeats rock";
             }
             else if (playerSelection == "paper" && computerSelection == "scissors") {
                computerScore += 1;
-               round += 1;
+               
                return "you lose, scissors defeats paper";
             }
            else if (playerSelection == "scissors" && computerSelection == "paper") {
                playerScore += 1;
-               round += 1;
+               
                return "you win, scissors defeats paper";
             }
             else if (playerSelection == "scissors" && computerSelection == "rock") {
                computerScore += 1;
-               round += 1;
+               
                return "you lose, rock defeats scissors";
             }
         
 }
 
 function playGame(){
+   let round = 0;
 
-   let playerSelection = prompt().toLowerCase();
-   let computerSelection = getComputerChoice();
+   for(let i = 1; i <= 5; i++) {
+         round++;
+         let playerSelection = prompt().toLowerCase();
+         let computerSelection = getComputerChoice();
+         console.log( "player chose " + playerSelection + ", " + "computer chose " + computerSelection );
 
-   console.log( "round " + round + ": " + "player chose " + playerSelection + ", " + playRound( playerSelection, computerSelection));
-   console.log( "computer score = " + computerScore + " , " + "player score = " + playerScore);
-     
-    if(playerScore >= 3 && round == 5) {
-      alert("player wins the game");
-    }
-    else if(computerScore >= 3 && round == 5){
-      alert("computer wins the game");
-    }
-   else if (playerScore < 3 && computerScore < 3 && round == 5) {
-      if(playerScore > computerScore) alert("player wins the game")
-      if(playerScore < computerScore) alert("computer wins the game")
-      if(playerScore == computerScore) alert("the game is a tie") 
-   };
+         playRound( playerSelection, computerSelection);
+
+         console.log( "round: " + round + "; computer score = " + computerScore + " , " + "player score = " + playerScore);
+
+   }
+ 
+  if(playerScore > computerScore && round == 5) {
+    alert("player wins the game");
+  }
+  else if(computerScore > playerScore && round == 5){
+    alert("computer wins the game");
+  }
+ else if (playerScore == computerScore && round == 5) {
+    alert("the game is a tie") 
+ };
 }
 
 playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-
 
 
